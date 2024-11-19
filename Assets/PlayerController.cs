87958625e5c42ef69283b2f484b58d629fcbf68e
flu0xetiina <cristina.garbayo@gmail.com>
2024-11-19 17:17:50 +0100

    el personaje se mueve y salta
    si, tambien salta, we cheered... ahora falta que el capsule collider funcione bien pero algo es algo jdr

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public CharacterController controlJugador;
+    public float gravityScale;
 
     private Vector3 moveDirection;
+    private float yStore;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +19,20 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   yStore = moveDirection.y;
+
+        //movimiento
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        transform.position = transform.position + (moveDirection * moveSpeed * Time.deltaTime);
+        moveDirection = moveDirection * moveSpeed;
+        controlJugador.Move(moveDirection * Time.deltaTime);
+        moveDirection.y = yStore;
+
+        //salto
+        if (Input.GetButtonDown("Jump")){
+            moveDirection.y = jumpForce;
+        }
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+        controlJugador.Move(moveDirection * Time.deltaTime);
     }
 }
