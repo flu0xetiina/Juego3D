02509fa16d9691f9ae9314cdce8c2b04c3c0e379
flu0xetiina <cristina.garbayo@gmail.com>
2024-11-22 20:36:25 +0100

    El personaje está animado
    increible, celebremos

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,27 +9,50 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 moveDirection;
     private float yStore;
+
+    // Reference to the Animator
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Ensure Animator is assigned
+        if (anim == null)
+        {
+            anim = GetComponent<Animator>();
+        }
     }
 
     // Update is called once per frame
     void Update()
-    {   yStore = moveDirection.y;
+    {   
+        // Store vertical movement
+        yStore = moveDirection.y;
 
-        //movimiento
+        // Movement
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         moveDirection = moveDirection * moveSpeed;
         controlJugador.Move(moveDirection * Time.deltaTime);
         moveDirection.y = yStore;
 
-        //salto
-        if (Input.GetButtonDown("Jump")){
+        // Check if the player is walking (moving on the X or Z axis)
+        bool isWalking = moveDirection.x != 0 || moveDirection.z != 0;
+
+        // Update the IsWalking animation parameter
+        anim.SetBool("isWalking", isWalking);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && controlJugador.isGrounded) // Ensure jumping only works when grounded
+        {
             moveDirection.y = jumpForce;
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
         }
 
+        // Gravity
         moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
         controlJugador.Move(moveDirection * Time.deltaTime);
     }
