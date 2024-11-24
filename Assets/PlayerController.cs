3c11fa58b26ecf36f4cbcd88670ca25f5d07917a
flu0xetiina <cristina.garbayo@gmail.com>
2024-11-24 21:57:02 +0100

    Siento que me muevo en círculos
    Estoy venga a tocar cositas del juego pero no llego a nada. Aun así, voy a hacer un commit por si acaso

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
 
     public Animator anim;
     public Camera playerCamera;  
+    public Transform spawnPoint;
+
 
     void Start()
     {
@@ -40,10 +42,31 @@ public class PlayerController : MonoBehaviour
            anim.SetTrigger("isJumping");
         }
 
+        
+
         // Gravity
         moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
         controlJugador.Move(moveDirection * Time.deltaTime);
 
         transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
     }
+
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.CompareTag("Water"))
+        {   Debug.Log("el personaje ha tocado agua");
+            Respawn();
+        }
+    }
+
+    // Método de respawn
+    void Respawn()
+    {
+        controlJugador.enabled = false; // Desactivar CharacterController temporalmente
+        transform.position = spawnPoint.position; // Mover al punto de respawn
+        controlJugador.enabled = true; // Reactivar CharacterController
+        moveDirection = Vector3.zero; // Resetear movimiento
+    }
+
 }
