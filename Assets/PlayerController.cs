87487fb8a9023b04aea0ee30f3cb16849f7c901b
flu0xetiina <cristina.garbayo@gmail.com>
2024-11-24 17:58:32 +0100

    giro del personaje

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,38 +10,30 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveDirection;
     private float yStore;
 
-    // Reference to the Animator
     public Animator anim;
+    public Camera playerCamera;  
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Ensure Animator is assigned
         if (anim == null)
         {
             anim = GetComponent<Animator>();
         }
     }
 
-    // Update is called once per frame
     void Update()
     {   
-        // Store vertical movement
         yStore = moveDirection.y;
 
-        // Movement
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         moveDirection = moveDirection * moveSpeed;
         controlJugador.Move(moveDirection * Time.deltaTime);
         moveDirection.y = yStore;
 
-        // Check if the player is walking (moving on the X or Z axis)
         bool isWalking = moveDirection.x != 0 || moveDirection.z != 0;
 
-        // Update the IsWalking animation parameter
         anim.SetBool("isWalking", isWalking);
 
-        // Jump
         if (Input.GetButtonDown("Jump")) 
         {
             moveDirection.y = jumpForce;
@@ -51,5 +43,7 @@ public class PlayerController : MonoBehaviour
         // Gravity
         moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
         controlJugador.Move(moveDirection * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
     }
 }
